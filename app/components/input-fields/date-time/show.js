38ea8283200dfb15c75f3a92f635adcd33779986
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-04-06 18:05:54 +0200

    fixed wrong mapping

--- app/components/input-fields/date-time/show.js
+++ app/components/input-fields/date-time/show.js
@@ -1,1 +1,1 @@
-export { default } from 'ember-submission-form-fields/components/input-fields/date/show';
+export { default } from 'ember-submission-form-fields/components/input-fields/date-time/show';
