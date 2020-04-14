1e372d2070d821b95e6962f88dfa7a703e50a715
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-14 16:44:57 +0200

    Remove ember- prefix from components subfolder

--- addon/helpers/component-for-display-type-show.js
+++ addon/helpers/component-for-display-type-show.js
@@ -2,14 +2,14 @@ import { helper } from '@ember/component/helper';
 
 export default helper(function componentForDisplayTypeShow(displayTypeUri) {
   const mapping = {
-    'http://lblod.data.gift/display-types/defaultInput' : `ember-submission-form/input-fields/input/show`,
-    'http://lblod.data.gift/display-types/textArea' : `ember-submission-form/input-fields/text-area/show`,
-    'http://lblod.data.gift/display-types/date' : `ember-submission-form/input-fields/date/show`,
-    'http://lblod.data.gift/display-types/dateTime' : `ember-submission-form/input-fields/date-time/show`,
-    'http://lblod.data.gift/display-types/files' : `ember-submission-form/input-fields/files/show`,
-    'http://lblod.data.gift/display-types/remoteUrls' : `ember-submission-form/input-fields/remote-urls/show`,
-    'http://lblod.data.gift/display-types/conceptSchemeSelector': `ember-submission-form/input-fields/concept-scheme-selector/show`,
-    'http://lblod.data.gift/display-types/vLabelOpcentiem': `ember-submission-form/input-fields/vlabel-opcentiem/show`
+    'http://lblod.data.gift/display-types/defaultInput' : `submission-form/input-fields/input/show`,
+    'http://lblod.data.gift/display-types/textArea' : `submission-form/input-fields/text-area/show`,
+    'http://lblod.data.gift/display-types/date' : `submission-form/input-fields/date/show`,
+    'http://lblod.data.gift/display-types/dateTime' : `submission-form/input-fields/date-time/show`,
+    'http://lblod.data.gift/display-types/files' : `submission-form/input-fields/files/show`,
+    'http://lblod.data.gift/display-types/remoteUrls' : `submission-form/input-fields/remote-urls/show`,
+    'http://lblod.data.gift/display-types/conceptSchemeSelector': `submission-form/input-fields/concept-scheme-selector/show`,
+    'http://lblod.data.gift/display-types/vLabelOpcentiem': `submission-form/input-fields/vlabel-opcentiem/show`
   };
 
   return mapping[displayTypeUri] || '';
