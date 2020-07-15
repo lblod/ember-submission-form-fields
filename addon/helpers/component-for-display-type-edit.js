e96b3e3b419032124e2fe2c036ecffa7eacb9f1f
Dries Hugaerts <dries.hugaerts@live.be>
2020-07-15 08:53:26 +0200

    linted component-for-display-type-edit.js

--- addon/helpers/component-for-display-type-edit.js
+++ addon/helpers/component-for-display-type-edit.js
@@ -2,9 +2,9 @@ import { helper } from '@ember/component/helper';
 
 export default helper(function componentForDisplayTypeEdit(displayTypeUri) {
   const mapping = {
-    'http://lblod.data.gift/display-types/defaultInput' : `submission-form/input-fields/input/edit`,
-    'http://lblod.data.gift/display-types/dateTime' : `submission-form/input-fields/date-time/edit`,
-    'http://lblod.data.gift/display-types/date' : `submission-form/input-fields/date/edit`,
+    'http://lblod.data.gift/display-types/defaultInput': `submission-form/input-fields/input/edit`,
+    'http://lblod.data.gift/display-types/dateTime': `submission-form/input-fields/date-time/edit`,
+    'http://lblod.data.gift/display-types/date': `submission-form/input-fields/date/edit`,
     'http://lblod.data.gift/display-types/textArea': `submission-form/input-fields/text-area/edit`,
     'http://lblod.data.gift/display-types/conceptSchemeSelector': `submission-form/input-fields/concept-scheme-selector/edit`,
     'http://lblod.data.gift/display-types/files': `submission-form/input-fields/files/edit`,
@@ -12,7 +12,7 @@ export default helper(function componentForDisplayTypeEdit(displayTypeUri) {
     'http://lblod.data.gift/display-types/vLabelOpcentiem': `submission-form/input-fields/vlabel-opcentiem/edit`,
     'http://lblod.data.gift/display-types/switch': `submission-form/input-fields/switch/edit`,
     'http://lblod.data.gift/display-types/conceptSchemeMultiSelector': `submission-form/input-fields/concept-scheme-multi-selector/edit`,
-    'http://lblod.data.gift/display-types/dateRange': `submission-form/input-fields/date-range/edit`
+    'http://lblod.data.gift/display-types/dateRange': `submission-form/input-fields/date-range/edit`,
   };
 
   return mapping[displayTypeUri] || '';
