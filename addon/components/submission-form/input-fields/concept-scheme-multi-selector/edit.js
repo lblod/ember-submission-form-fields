3a827fa26458b5e384635c50c4403a4805efa1d3
Dries Hugaerts <dries.hugaerts@live.be>
2020-07-15 08:51:28 +0200

    linted concept-scheme-multi-selector edit.js

--- addon/components/submission-form/input-fields/concept-scheme-multi-selector/edit.js
+++ addon/components/submission-form/input-fields/concept-scheme-multi-selector/edit.js
@@ -2,10 +2,10 @@ import InputFieldComponent from '../input-field';
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import { tracked } from '@glimmer/tracking';
-import { triplesForPath, updateSimpleFormValue} from '@lblod/submission-form-helpers';
+import { triplesForPath, updateSimpleFormValue } from '@lblod/submission-form-helpers';
 import { SKOS } from '@lblod/submission-form-helpers';
-import {timeout} from 'ember-concurrency';
-import {task, restartableTask} from 'ember-concurrency-decorators';
+import { timeout } from 'ember-concurrency';
+import { restartableTask } from 'ember-concurrency-decorators';
 import rdflib from 'browser-rdflib';
 
 function byLabel(a, b) {
@@ -17,8 +17,8 @@ function byLabel(a, b) {
 export default class FormInputFieldsConceptSchemeSelectorEditComponent extends InputFieldComponent {
   inputId = 'select-' + guidFor(this);
 
-  @tracked selected = null
-  @tracked options = []
+  @tracked selected = null;
+  @tracked options = [];
 
   get subset() {
     return this.options.slice(0, 50);
@@ -30,17 +30,16 @@ export default class FormInputFieldsConceptSchemeSelectorEditComponent extends I
     this.loadProvidedValue();
   }
 
-  loadOptions(){
+  loadOptions() {
     const metaGraph = this.args.graphs.metaGraph;
     const fieldOptions = JSON.parse(this.args.field.options);
     const conceptScheme = new rdflib.namedNode(fieldOptions.conceptScheme);
 
-    this.options = this.args.formStore
-      .match(undefined, SKOS('inScheme'), conceptScheme, metaGraph)
-      .map(t => {
-        const label = this.args.formStore.any(t.subject, SKOS('prefLabel'), undefined, metaGraph);
-        return { subject: t.subject, label: label && label.value };
-      });
+    this.options = this.args.formStore.match(undefined, SKOS('inScheme'), conceptScheme, metaGraph)
+    .map(t => {
+      const label = this.args.formStore.any(t.subject, SKOS('prefLabel'), undefined, metaGraph);
+      return {subject: t.subject, label: label && label.value};
+    });
     this.options.sort(byLabel);
   }
 
@@ -52,7 +51,7 @@ export default class FormInputFieldsConceptSchemeSelectorEditComponent extends I
   }
 
   @action
-  updateSelection(options){
+  updateSelection(options) {
     this.selected = options;
 
     // Cleanup old value(s) in the store
@@ -62,7 +61,7 @@ export default class FormInputFieldsConceptSchemeSelectorEditComponent extends I
 
     // Insert new value in the store
     if (options) {
-      options.forEach(option => updateSimpleFormValue(this.storeOptions, option.subject))
+      options.forEach(option => updateSimpleFormValue(this.storeOptions, option.subject));
     }
 
     this.hasBeenFocused = true;
