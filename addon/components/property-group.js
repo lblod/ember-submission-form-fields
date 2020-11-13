1e92e46a2ed7803a7cfa86f1cafeee9a85caef5d
Dries Hugaerts <dries.hugaerts@live.be>
2020-11-13 15:51:36 +0100

    improved the way that the property-group component rerenders it's children.

--- addon/components/property-group.js
+++ addon/components/property-group.js
@@ -3,8 +3,6 @@ import { tracked } from '@glimmer/tracking';
 import { A } from '@ember/array';
 import { getChildrenForPropertyGroup } from '../utils/model-factory';
 import { guidFor } from '@ember/object/internals';
-import { removeTriples } from '@lblod/submission-form-helpers';
-import { isPropertyGroup } from '../helpers/is-property-group';
 
 export default class SubmissionFormPropertyGroupComponent extends Component {
 
@@ -14,8 +12,6 @@ export default class SubmissionFormPropertyGroupComponent extends Component {
 
   constructor() {
     super(...arguments);
-    // this.register();
-
     this.update(
       this.args.group,
       {
@@ -23,9 +19,6 @@ export default class SubmissionFormPropertyGroupComponent extends Component {
         store: this.args.formStore,
         graphs: this.args.graphs,
         node: this.args.sourceNode,
-      },
-      {
-        cacheConditionals: this.args.cacheConditionals,
       },
     );
   }
@@ -44,9 +37,6 @@ export default class SubmissionFormPropertyGroupComponent extends Component {
           graphs: this.args.graphs,
           node: this.args.sourceNode,
         },
-        {
-          cacheConditionals: this.args.cacheConditionals,
-        },
       );
     }, this.observerLabel);
   }
@@ -55,35 +45,29 @@ export default class SubmissionFormPropertyGroupComponent extends Component {
     this.args.formStore.deregisterObserver(this.observerLabel);
   }
 
-
-  update(group, {form, store, graphs, node}, options) {
+  update(group, {form, store, graphs, node}) {
     this.deregister(); // NOTE: to prevent calling ourself up again with changes
 
-    console.log(`DEBUG: started updating PropertyGroup "${group.description}" ...`);
     // 1) retrieve the to be rendered children (!!could be nested property-groups or fields) for this property-group
     const children = getChildrenForPropertyGroup(group, {form, store, graphs, node});
-    console.log(`DEBUG: ${children.length} found that should be rendered (current state)`);
 
     // 2) calculate to be removed
     const deletes = this.children.filter(rendered => !children.find(child => child.uri.equals(rendered.uri)));
-    console.log(`DEBUG: ${deletes.length} children marked for removal`);
 
-    // 3) calculate children to be inserted
-    const inserts = children.map((child, index) => {
-      return {index, value: child};
-    }).filter(insert => !!!this.children.find(rendered => rendered.uri.equals(insert.value.uri)));
-    console.log(`DEBUG: ${inserts.length} children found that are not yet rendered `);
-
-    // 4) update the values in the rdflib-store
-    if (deletes.length && !options.cacheConditionals) {
-      deletes.filter(child => !isPropertyGroup(child.displayType)) // NOTE only fields can contain values
-        .forEach((field) => {
-          removeTriples({store, path: field.rdflibPath, sourceNode: node, sourceGraph: graphs.sourceGraph});
-        });
+    // 4) remove the "unwanted" children
+    if (deletes.length) {
+      this.children.removeObjects(deletes);
     }
-    // 5) update the rendered children
-    this.children.removeObjects(deletes);
-    inserts.forEach(insert => this.children.replace(insert.index, 1, [insert.value]));
+
+    // 5) insert new fields or move old fields accordingly
+    children.forEach((field, i) => {
+      const existingField = this.children.find(eField => eField.uri.equals(field.uri));
+      if (existingField) {
+        this.children.replace(i, 1, [existingField]);
+      } else {
+        this.children.replace(i, 1, [field]);
+      }
+    });
 
     this.register(); // NOTE: to make sure we get notified on user input
   }
