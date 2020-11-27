634b38957386f0492aee86619b7e3c46e82e44a0
Dries Hugaerts <dries.hugaerts@live.be>
2020-11-27 16:33:20 +0100

    improved numerical-input to push correct datatypes to the store.

--- addon/components/rdf-input-fields/numerical-input/edit.js
+++ addon/components/rdf-input-fields/numerical-input/edit.js
@@ -1,6 +1,8 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '../simple-value-input-field';
+import rdflib from 'browser-rdflib';
+import { XSD } from '@lblod/submission-form-helpers';
 
 export default class RdfInputFieldsNumericalInputEditComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
@@ -8,7 +10,19 @@ export default class RdfInputFieldsNumericalInputEditComponent extends SimpleInp
   @action
   updateValue(e) {
     e.preventDefault();
-    const numericalValue = parseInt(this.value);
-    numericalValue ? super.updateValue(numericalValue) : super.updateValue(this.value && this.value.trim());
+    const number = rdflib.literal(this.value, this.datatype);
+    super.updateValue(number);
+  }
+
+  get datatype() {
+    const number = parseFloat(this.value);
+    if(!isNaN(number)) {
+      if (Number.isInteger(number)) {
+        return XSD('integer');
+      }
+      return XSD('decimal');
+    }
+    // NOTE shouldn't be able to reach this point
+    return XSD('string');
   }
 }
