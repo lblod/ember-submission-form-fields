b14c9c989ca9b894894e772f6e47ee59262151c3
Dries Hugaerts <dries.hugaerts@live.be>
2021-01-26 14:58:25 +0100

    Improved concept-scheme-radio-buttons/edit to make use off provided functionality in simple-input-field-component.

--- addon/components/rdf-input-fields/concept-scheme-radio-buttons/edit.js
+++ addon/components/rdf-input-fields/concept-scheme-radio-buttons/edit.js
@@ -3,7 +3,6 @@ import { guidFor } from '@ember/object/internals';
 
 import SimpleInputFieldComponent from '../simple-value-input-field';
 import { tracked } from '@glimmer/tracking';
-import { triplesForPath, updateSimpleFormValue} from '@lblod/submission-form-helpers';
 import { SKOS } from '@lblod/submission-form-helpers';
 import rdflib from 'browser-rdflib';
 
@@ -17,12 +16,10 @@ function byLabel(a, b) {
 export default class RdfInputFieldsconceptSchemeRadioButtonsEditComponent extends SimpleInputFieldComponent {
   inputId = 'conceptSchemeRadioButtons-' + guidFor(this);
 
-  @tracked selected = null
   @tracked options = []
   constructor() {
     super(...arguments);
     this.loadOptions();
-    this.loadProvidedValue();
   }
 
   loadOptions(){
@@ -32,35 +29,14 @@ export default class RdfInputFieldsconceptSchemeRadioButtonsEditComponent extend
     this.options = this.args.formStore
       .match(undefined, SKOS('inScheme'), conceptScheme, metaGraph)
       .map(t => {
-        const label = this.args.formStore.any(t.subject, SKOS('prefLabel'), undefined, metaGraph);
-        return { subject: t.subject, label: label && label.value };
+        const label = this.args.formStore.any( t.subject, SKOS('prefLabel'), undefined, metaGraph);
+        return { value: t.subject.value, nodeValue: t.subject, label: label && label.value };
       });
     this.options.sort(byLabel);
   }
 
-  loadProvidedValue() {
-    const matches = triplesForPath(this.storeOptions);
-    if (matches.values.length > 0) {
-      this.nodeValue = matches.values[0];
-      this.value = matches.values[0].value === "1";
-    } 
-  }
-
   @action
-  updateSelection(option){
-    this.selected = option;
-
-    // Cleanup old value(s) in the store
-    const matches = triplesForPath(this.storeOptions, true).values;
-    const matchingOptions = matches.filter(m => this.options.find(opt => m.equals(opt.subject)));
-    matchingOptions.forEach(m => updateSimpleFormValue(this.storeOptions, undefined, m));
-
-    // Insert new value in the store
-    if (option) {
-      updateSimpleFormValue(this.storeOptions, option.subject);
-    }
-
-    this.hasBeenFocused = true;
-    super.updateValidations();
+  updateValue(option) {
+    super.updateValue(option.nodeValue);
   }
 }
