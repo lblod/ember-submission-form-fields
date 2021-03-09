89670e450c64dc31c89b8a111517c81f3c80ff88
Claire Lovisa <lovisa.claire@gmail.com>
2021-03-09 12:16:01 +0100

    Add usage of a defaultValue param in some inputs

--- addon/components/rdf-input-fields/input/edit.js
+++ addon/components/rdf-input-fields/input/edit.js
@@ -1,13 +1,52 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '../simple-value-input-field';
+import { FORM, SHACL } from '@lblod/submission-form-helpers';
+import { next } from '@ember/runloop';
 
 export default class FormInputFieldsInputEditComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
 
+  constructor() {
+    super(...arguments);
+
+    if (!this.value) {
+      this.setDefaultValue();
+      next(this, () => {
+        if (this.value) {
+          this.updateFieldValue();
+        }
+      });
+    }
+  }
+
   @action
   updateValue(e) {
     e.preventDefault();
+    this.updateFieldValue();
+  }
+
+  updateFieldValue() {
     super.updateValue(this.value && this.value.trim());
+  }
+
+  /**
+   * Sets a default value on the field if the property `form:defaultValue` is defined in the
+   * field's configuration
+   */
+  setDefaultValue() {
+    const field = this.storeOptions.store.match(
+      undefined,
+      SHACL('path'),
+      this.storeOptions.path,
+      this.storeOptions.formGraph)[0].subject;
+
+    const defaultValueTriple = this.storeOptions.store.match(
+      field,
+      FORM('defaultValue'),
+      undefined,
+      this.storeOptions.formGraph)[0];
+
+    if (defaultValueTriple) this.value = defaultValueTriple.object.value;
   }
 }
--- addon/components/rdf-input-fields/numerical-input/edit.js
+++ addon/components/rdf-input-fields/numerical-input/edit.js
@@ -2,14 +2,32 @@ import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '../simple-value-input-field';
 import rdflib from 'browser-rdflib';
-import { XSD } from '@lblod/submission-form-helpers';
+import { XSD, FORM, SHACL } from '@lblod/submission-form-helpers';
+import { next } from '@ember/runloop';
 
 export default class RdfInputFieldsNumericalInputEditComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
 
+  constructor() {
+    super(...arguments);
+
+    if (!this.value) {
+      this.setDefaultValue();
+      next(this, () => {
+        if (this.value) {
+          this.updateFieldValue();
+        }
+      });
+    }
+  }
+
   @action
   updateValue(e) {
     e.preventDefault();
+    this.updateFieldValue();
+  }
+
+  updateFieldValue() {
     const number = rdflib.literal(Number(this.value), this.datatype);
     super.updateValue(number);
   }
@@ -25,5 +43,25 @@ export default class RdfInputFieldsNumericalInputEditComponent extends SimpleInp
     }
     // NOTE: everything that is not a number is a string.
     return XSD('string');
+  }
+
+  /**
+   * Sets a default value on the field if the property `form:defaultValue` is defined in the
+   * field's configuration
+   */
+  setDefaultValue() {
+    const field = this.storeOptions.store.match(
+      undefined,
+      SHACL('path'),
+      this.storeOptions.path,
+      this.storeOptions.formGraph)[0].subject;
+
+    const defaultValueTriple = this.storeOptions.store.match(
+      field,
+      FORM('defaultValue'),
+      undefined,
+      this.storeOptions.formGraph)[0];
+
+    if (defaultValueTriple) this.value = defaultValueTriple.object.value;
   }
 }
