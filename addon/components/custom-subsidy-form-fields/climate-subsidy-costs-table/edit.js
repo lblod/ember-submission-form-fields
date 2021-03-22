518d241dfbed6977cf69d7babf8f99bcc4f1b705
Unknown <achrafatauil@hotmail.com>
2021-03-22 12:59:37 +0100

    add uuid key to tableRows objects

--- addon/components/custom-subsidy-form-fields/climate-subsidy-costs-table/edit.js
+++ addon/components/custom-subsidy-form-fields/climate-subsidy-costs-table/edit.js
@@ -42,120 +42,140 @@ function inputCost(count) {
 
 const tableRows = [
   {
+    uuid: "0f4e3cff-4fb8-4892-9dca-1d582a433c77",
     description: 'Algemene beleidsdoelstellingen: Ondersteuning Burgemeestersconvenant2030',
     cost: '15ct per inwoner met een maximum plafond van 20.000 €',
     amountPerAction: inputCost(citizenCount),
     index: 1
   },
   {
+    uuid: "6469df9c-b933-45c3-9457-4958c33935ca",
     description: 'Algemene beleidsdoelstellingen: Ondersteuning Strategisch Vastgoedplan',
     cost: citizenBasedCost(citizenCount),
     amountPerAction: 0,
     index: 2
   },
   {
+    uuid: "286c35b3-4817-4b33-9ad5-828747610b2a",
     description: 'Algemene beleidsdoelstellingen: Technische assistentie aanbestedingen publiek patrimonium of klimaatwijken met hefboom 1:10',
     cost: 'nvt',
     amountPerAction: 0,
     index: 3
   },
   {
+    uuid: "384b2567-ab54-4b6f-b19c-a438829b3666",
     description: 'Werf 1 - Vergroening: Investeringssteun voor het planten van bomen op publiek domein',
     cost: 150.00,
     amountPerAction: 0,
     index: 4
   },
   {
+    uuid: "e6339d50-0969-4911-924c-bb0c629c7b00",
     description: 'Werf 1 - Vergroening: Investeringssteun voor het planten van bomen op privaat domein (door particulieren, verenigingen, KMO"s)',
     cost: 50.00,
     amountPerAction: 0,
     index: 5
   },
   {
+    uuid: "396fe1bd-0a13-4823-8824-05ea3a527337",
     description: 'Werf 1 - Vergroening: Investeringssteun voor het planten van hagen (per meter) op publiek domein',
     cost: 13.50,
     amountPerAction: 0,
     index: 6
   },
   {
+    uuid: "75ad483c-b7cb-411c-b9b1-07af31bfc0e6",
     description: 'Werf 1 - Vergroening: Investeringssteun voor het planten van hagen (per meter) op privaat domein (door particulieren, verenigingen, KMO"s)',
     cost: 5.00,
     amountPerAction: 0,
     index: 7
   },
   {
+    uuid: "75b3a2d2-bf33-49e7-9ced-c166213970bb",
     description: 'Werf 1 - Vergroening: Investeringssteun geveltuinbeplanting (type geveltuin, per meter) door particulieren',
     cost: 38.50,
     amountPerAction: 0,
     index: 8
   },
   {
+    uuid: "567c8492-c587-4876-b910-f6bbcd25c971",
     description: 'Werf 1 - Vergroening: Investeringssteun aanleg natuurgroenperk van minimaal 10m2 op openbaar toegankelijk domein',
     cost: 540.00,
     amountPerAction: 0,
     index: 9
   },
   {
+    uuid: "0eb27f0d-bbe3-41c1-8793-b1b13b1b8715",
     description: 'Werf 2 - Verrijk je wijk: Ondersteuning traject collectieve renovaties (per wooneenheid)',
     cost: 75.00,
     amountPerAction: 0,
     index: 10
   },
   {
+    uuid: "23e94ba7-57cf-4ad4-8f5e-b4a8dcc6f816",
     description: 'Werf 2 - Verrijk je wijk: Aanbesteding participatieve hernieuwbare energie (per 18kWp)',
     cost: 990.00,
     amountPerAction: 0,
     index: 11
   },
   {
+    uuid: "38d6d2bd-e42b-4d7e-8fea-9a371d9cf22f",
     description: 'Werf 2 - Verrijk je wijk: Opstarttraject lokale energiegemeenschap',
     cost: 20000.00,
     amountPerAction: 0,
     index: 12
   },
   {
+    uuid: "3b939085-ea60-468e-9fb8-ee0b54f1d73c",
     description: 'Werf 3 - Iedere buurt deelt: Jaar 1 afname garantie per jaar per toegangspunt voor koolstofvrije deelsystemen (1 toegangspunt leidt tot 2 deelwagens)',
     cost: 12000.00,
     amountPerAction: 0,
     index: 13
   },
   {
+    uuid: "e460d281-8b5b-445e-a34d-2a6cb9c10675",
     description: 'Werf 3 - Iedere buurt deelt: Jaar 2 afname garantie per jaar per toegangspunt voor koolstofvrije deelsystemen (1 toegangspunt leidt tot 2 deelwagens)',
     cost: 8400.00,
     amountPerAction: 0,
     index: 14
   },
   {
+    uuid: "49150951-e112-4649-a573-475fd3f22e99",
     description: 'Werf 3 - Iedere buurt deelt: Jaar 3 afname garantie per jaar per toegangspunt voor koolstofvrije deelsystemen (1 toegangspunt leidt tot 2 deelwagens)',
     cost: 4800.00,
     amountPerAction: 0,
     index: 15
   },
   {
+    uuid: "d66a5dbc-8913-4ccf-92bf-a4b62f3d5c58",
     description: 'Werf 3 - Iedere buurt deelt: Promotiecampagne per 2 nieuwe toegangspunten',
     cost: 7500.00,
     amountPerAction: 0,
     index: 16
   },
   {
+    uuid: "f4956772-fd3e-48f5-b546-e5298fff78ad",
     description: 'Werf 4 - Water het nieuwe goud: Investeringssteun per 1 m² ontharding publiek domein',
     cost: 50.00,
     amountPerAction: 0,
     index: 17
   },
   {
+    uuid: "0efb6b57-2ab4-4526-b71e-ec3568f01d1b",
     description: 'Werf 4 - Water het nieuwe goud: investeringssteun per 1 m² ontharding privaat domein',
     cost: 35.00,
     amountPerAction: 0,
     index: 18
   },
   {
+    uuid: "1c4fbee8-1b18-4c1a-9a05-a3b20a5eae94",
     description: 'Werf 4 - Water het nieuwe goud: investeringssteun per 1 m³ hemelwaterput + infiltratievoorziening in de bebouwde omgeving',
     cost: 500.00,
     amountPerAction: 0,
     index: 19
   },
   {
+    uuid: "5d485edc-3352-4bbe-a74c-e50cc0e22dec",
     description: 'Werf 4 - Water het nieuwe goud: investeringssteun per 1 m³ hemelwaterbuffer op openbaar toegankelijk domein',
     cost: 1000.00,
     amountPerAction: 0,
