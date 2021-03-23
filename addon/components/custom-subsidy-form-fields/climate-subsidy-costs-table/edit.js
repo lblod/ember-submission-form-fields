8a733c35c18cd428d8617c356e7c82b8795ccc58
Unknown <achrafatauil@hotmail.com>
2021-03-23 16:43:51 +0100

    cleanup edit.js

--- addon/components/custom-subsidy-form-fields/climate-subsidy-costs-table/edit.js
+++ addon/components/custom-subsidy-form-fields/climate-subsidy-costs-table/edit.js
@@ -1,6 +1,5 @@
 import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import { triplesForPath } from '@lblod/submission-form-helpers';
 import { next } from '@ember/runloop';
 import rdflib from 'browser-rdflib';
@@ -29,17 +28,6 @@ export default class CustomSubsidyFormFieldsClimateSubsidyCostsTableEditComponen
         this.storeOptions.sourceGraph).length > 0;
   }
 
-  get populationCount() {
-    // return { this.storeOptions.store.match(
-    //   this.storeOptions.sourceNode,
-    //   populationCountPredicate,
-    //   undefined,
-    //   this.storeOptions.sourceGraph
-    // }
-
-    // TODO: Set `Aantal inwoners` input to correct value from DB, then return that value for this getter
-    return 90000;
-  }
 
   constructor() {
     super(...arguments);
@@ -90,46 +78,6 @@ export default class CustomSubsidyFormFieldsClimateSubsidyCostsTableEditComponen
     }
     ];
     this.storeOptions.store.addAll(triples);
-  }
-
-
-  //TODO some part needs to be moved of the following functions
-
-  // set right amount based on conditions (row 2)
-  checkCostForPopulation(index, cost, population) {
-    if (index == 2) {
-      if (population < 25000) return 15000;
-      if (population > 25000 && population < 100000) return 40000;
-      if (population > 100000) return 60000;
-    } else {
-      return cost;
-    }
-  }
-
-  // set right amount based on calculation (row 1)
-  checkActionForPopulation(index, count, population) {
-    if (index == 1){
-      const calculated = Math.round(0.15 * population);
-      if (calculated > 20000) return 20000;
-      return calculated;
-    } else {
-      return count;
-    }
-  }
-
-  // Check if 'Te realiseren eenheden' is conditional (applies for row 1 & 2)
-  checkEenhedenConditions(index, amountPerAction, costPerUnit) {
-    if (index == 1 && amountPerAction > 0) {
-      return "1 goedgekeurd SECAP2030";
-    } else if (index == 2 && amountPerAction > 0) {
-      return "1 strategisch vastgoedplan publiek patrimonium";
-    } else if (index == 3) {
-      return "/";
-    } else if (amountPerAction <= 0) {
-      return 0;
-    } else {
-      return (amountPerAction / parseInt(costPerUnit)).toFixed(2);
-    }
   }
 
   isSmallerThan(value, max) {
