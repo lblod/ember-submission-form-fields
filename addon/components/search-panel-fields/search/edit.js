7f097acdcb9e3a80043e08cb678b4df4bb8c4b4e
Dries Hugaerts <dries.hugaerts@live.be>
2021-11-16 14:46:54 +0100

    Small fix to prevent deprecation notices on initialization of the free-text-search.

--- addon/components/search-panel-fields/search/edit.js
+++ addon/components/search-panel-fields/search/edit.js
@@ -3,14 +3,19 @@ import { tracked } from '@glimmer/tracking';
 import SimpleInputFieldComponent from '../../rdf-input-fields/simple-value-input-field';
 import { timeout } from 'ember-concurrency';
 import { restartableTask } from 'ember-concurrency-decorators';
+import { scheduleOnce } from '@ember/runloop';
 
 export default class FormSearchPanelFieldsSearchEditComponent extends SimpleInputFieldComponent {
   inputId = 'search-' + guidFor(this);
 
   @tracked _freeTextSearch = null;
 
-  loadProvidedValue() {
-    super.loadProvidedValue();
+  constructor() {
+    super(...arguments);
+    scheduleOnce('afterRender', this, 'setFreeTextSearch');
+  }
+
+  setFreeTextSearch() {
     if (!this._freeTextSearch) {
       this._freeTextSearch = this.value;
     }
