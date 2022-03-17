c809db7c79ec4805443c91a1bc4b757077c3be81
Unknown <achrafatauil@hotmail.com>
2022-03-17 15:03:16 +0100

    create table-row component
    still some hardcoded stuff that needs to disappear

---
+++ addon/components/custom-subsidy-form-fields/plan-living-together-table/table-row.js
@@ -0,0 +1,295 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import rdflib from 'browser-rdflib';
+import { scheduleOnce } from '@ember/runloop';
+import { v4 as uuidv4 } from 'uuid';
+import { RDF, XSD } from '@lblod/submission-form-helpers';
+
+const MU = new rdflib.Namespace('http://mu.semte.ch/vocabularies/core/');
+
+const planBaseUri = 'http://lblod.data.gift/vocabularies/subsidie/plan-samenleven/';
+const planTableBaseUri = 'http://data.lblod.info/plan-living-together-tables';
+
+const tableEntryBaseUri = 'http://data.lblod.info/id/plan-living-together-table/row-entry';
+const PlanEntryType = new rdflib.NamedNode(`${planBaseUri}PlanLivingTogetherEntry`);
+const planEntryPredicate = new rdflib.NamedNode(`${planBaseUri}planLivingTogetherEntry`);
+const descriptionPredicate = new rdflib.NamedNode(`${planBaseUri}description`);
+const currentRangePredicate = new rdflib.NamedNode(`${planBaseUri}currentRange`);
+const plannedRangePredicate = new rdflib.NamedNode(`${planBaseUri}plannedRange`);
+const contributionPredicate = new rdflib.NamedNode(`${planBaseUri}contribution`);
+const priorityPredicate = new rdflib.NamedNode(`${planBaseUri}priority`);
+const hasInvalidRowPredicate = new rdflib.NamedNode(`${planTableBaseUri}hasInvalidPlanLivingTogetherTableEntry`);
+
+export default class CustomSubsidyFormFieldsPlanLivingTogetherTableTableRowComponent extends Component {
+  @tracked tableEntryUri = null;
+  @tracked currentRange = null;
+  @tracked plannedRange = null;
+  @tracked priority = null;
+
+  @tracked currentRangeErrors = [];
+  @tracked plannedRangeErrors = [];
+  @tracked priorityErrors = [];
+
+
+  get storeOptions() {
+    return this.args.storeOptions;
+  }
+
+  get planTableSubject() {
+    return this.args.planTableSubject;
+  }
+
+  get businessRuleUri() {
+    return new rdflib.NamedNode(this.args.businessRuleUriStr);
+  }
+
+  get onUpdateRow(){
+    return this.args.onUpdateRow;
+  }
+
+  get multiplier() {
+    return this.args.multiplier;
+  }
+
+  get maxRange() {
+    return this.args.maxRange;
+  }
+
+  get contribution() {
+    return this.plannedRange * this.multiplier;
+  }
+
+  constructor() {
+    super(...arguments);
+    scheduleOnce("actions", this, this.initializeTableRow);
+  }
+
+  initializeTableRow() {
+    if (this.hasValues()) {
+      this.loadProvidedValue();
+      this.onUpdateRow();
+    }
+    else {
+      this.initializeDefault();
+    }
+  }
+
+  hasValues() {
+    const values = this.storeOptions.store.match(null, descriptionPredicate, this.businessRuleUri, this.storeOptions.sourceGraph);
+    return values.length;
+  }
+
+  loadProvidedValue() {
+    const values = this.storeOptions.store.match(null, descriptionPredicate, this.businessRuleUri, this.storeOptions.sourceGraph);
+    if (values.length > 1) {
+      throw `Expected single value for ${this.businessRuleUri}`;
+    }
+    else {
+      this.setComponentValues(values[0].subject);
+    }
+  }
+
+  setComponentValues(subject) {
+    this.tableEntryUri = subject;
+    this.currentRange = this.storeOptions.store.match(this.tableEntryUri, currentRangePredicate, null, this.storeOptions.sourceGraph)[0].object.value;
+    this.plannedRange = this.storeOptions.store.match(this.tableEntryUri, plannedRangePredicate, null, this.storeOptions.sourceGraph)[0].object.value;
+    console.log(this.plannedRange)
+    this.priority = this.storeOptions.store.match(this.tableEntryUri, priorityPredicate, null, this.storeOptions.sourceGraph)[0].object.value;
+  }
+
+  initializeDefault() {
+    const uuid = uuidv4();
+    const tableEntryUri = new rdflib.NamedNode(`${tableEntryBaseUri}/${uuid}`);
+
+    let triples = [
+      {
+        subject: tableEntryUri,
+        predicate: RDF('type'),
+        object: PlanEntryType,
+        graph: this.storeOptions.sourceGraph
+      },
+      {
+        subject: tableEntryUri,
+        predicate: MU('uuid'),
+        object: uuid,
+        graph: this.storeOptions.sourceGraph
+      },
+      {
+        subject: this.planTableSubject,
+        predicate: planEntryPredicate,
+        object: tableEntryUri,
+        graph: this.storeOptions.sourceGraph
+      },
+      {
+        subject: tableEntryUri,
+        predicate: descriptionPredicate,
+        object: this.businessRuleUri,
+        graph: this.storeOptions.sourceGraph
+      }
+    ];
+
+    triples.push(
+      {
+        subject: tableEntryUri,
+        predicate: currentRangePredicate,
+        object: null,
+        graph: this.storeOptions.sourceGraph
+      }
+    );
+
+    triples.push(
+      {
+        subject: tableEntryUri,
+        predicate: plannedRangePredicate,
+        object: 0,
+        graph: this.storeOptions.sourceGraph
+      }
+    );
+
+    triples.push(
+      {
+        subject: tableEntryUri,
+        predicate: priorityPredicate,
+        object: 0,
+        graph: this.storeOptions.sourceGraph
+      }
+    );
+
+    this.storeOptions.store.addAll(triples);
+    this.setComponentValues(tableEntryUri);
+  }
+
+  updateTripleObject(subject, predicate, newObject = null) {
+    const triples = this.storeOptions.store.match(
+      subject,
+      predicate,
+      undefined,
+      this.storeOptions.sourceGraph
+    );
+
+    this.storeOptions.store.removeStatements([...triples]);
+
+    if (newObject) {
+      this.storeOptions.store.addAll([
+        {
+          subject: subject,
+          predicate: predicate,
+          object: newObject,
+          graph: this.storeOptions.sourceGraph
+        }
+      ]);
+    }
+  }
+
+  validateCurrentRange(currentRange) {
+    this.currentRangeErrors = [];
+
+    if (!this.isPositiveInteger(currentRange)) {
+      this.currentRangeErrors.pushObject({
+        message: 'De waarde moet groter of gelijk aan 0 zijn.'
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+
+    else if (!this.isValidInteger(currentRange)) {
+      this.currentRangeErrors.pushObject({
+        message: 'De waarde moet een geheel getal vormen.'
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+  }
+
+  validatePlannedRange(plannedRange) {
+    this.plannedRangeErrors = [];
+
+    if (!this.isPositiveInteger(plannedRange)) {
+      this.plannedRangeErrors.pushObject({
+        message: 'De waarde moet groter of gelijk aan 0 zijn.'
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+
+    else if (!this.isValidInteger(plannedRange)) {
+      this.plannedRangeErrors.pushObject({
+        message: 'De waarde moet een geheel getal vormen.'
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+
+    else if (plannedRange > this.maxRange) {
+      this.plannedRangeErrors.pushObject({
+        message: `De waarde mag niet hoger liggen dan ${this.maxRange}.`
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+  }
+
+  validatePriority(priority) {
+    this.priorityErrors = [];
+
+    if (this.plannedRange > 0 && this.priority == 0) {
+      this.priorityErrors.pushObject({
+        message: 'Gelieve een waarde groter dan 0 in te geven.'
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+
+    else if (!this.isPositiveInteger(priority)) {
+      this.priorityErrors.pushObject({
+        message: 'De waarde moet groter of gelijk aan 0 zijn.'
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+
+    else if (!this.isValidInteger(priority)) {
+      this.priorityErrors.pushObject({
+        message: 'De waarde moet een geheel getal vormen.'
+      });
+      this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, this.tableEntryUri);
+      return false;
+    }
+
+  }
+
+  @action
+  update(e) {
+    if (e && typeof e.preventDefault === "function") e.preventDefault();
+
+    this.updateTripleObject(this.planTableSubject, hasInvalidRowPredicate, null);
+
+    /** start validation **/
+    this.validateCurrentRange(this.currentRange);
+    this.validatePlannedRange(this.plannedRange);
+    this.validatePriority(this.priority);
+
+    if (this.currentRangeErrors.length) return this.onUpdateRow();
+    if (this.plannedRangeErrors.length) return this.onUpdateRow();
+    if (this.priorityErrors.length) return this.onUpdateRow();
+    /** end validation **/
+
+    this.updateTripleObject(this.tableEntryUri, currentRangePredicate, rdflib.literal(this.currentRange, XSD('integer')));
+    this.updateTripleObject(this.tableEntryUri, plannedRangePredicate, rdflib.literal(this.plannedRange, XSD('integer')));
+    this.updateTripleObject(this.tableEntryUri, contributionPredicate, rdflib.literal(this.contribution, XSD('float')));
+    this.updateTripleObject(this.tableEntryUri, priorityPredicate, rdflib.literal(this.priority, XSD('integer')));
+    this.setComponentValues(this.tableEntryUri);
+
+    return this.onUpdateRow();
+  }
+
+  isPositiveInteger(value) {
+    return parseInt(value) >= 0;
+  }
+
+  isValidInteger(value) {
+    return parseFloat(value) % 1 === 0;
+  }
+
+}
