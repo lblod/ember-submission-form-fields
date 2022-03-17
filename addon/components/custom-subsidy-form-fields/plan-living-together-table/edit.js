4273d793f8607dadb230ea24c59cece0c32c4813
Unknown <achrafatauil@hotmail.com>
2022-03-17 15:04:13 +0100

    create edit component
    At the moment only 1 table row
    some extra info missing like the list above the table and some reminders on the bottom (see miro)

---
+++ addon/components/custom-subsidy-form-fields/plan-living-together-table/edit.js
@@ -0,0 +1,142 @@
+import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { triplesForPath } from '@lblod/submission-form-helpers';
+import { scheduleOnce } from '@ember/runloop';
+import rdflib from 'browser-rdflib';
+import { v4 as uuidv4 } from 'uuid';
+import { RDF } from '@lblod/submission-form-helpers';
+
+const MU = new rdflib.Namespace('http://mu.semte.ch/vocabularies/core/');
+
+const planBaseUri = 'http://lblod.data.gift/vocabularies/subsidie/plan-samenleven/';
+const planTableBaseUri = 'http://data.lblod.info/plan-living-together-tables';
+
+const lblodSubsidieBaseUri = 'http://lblod.data.gift/vocabularies/subsidie/';
+const planTableType = new rdflib.NamedNode(`${lblodSubsidieBaseUri}PlanLivingTogetherTablee`);
+const planTablePredicate = new rdflib.NamedNode(`${lblodSubsidieBaseUri}planLivingTogetherTable`);
+const plannedRangePredicate = new rdflib.NamedNode(`${planBaseUri}plannedRange`);
+const hasInvalidRowPredicate = new rdflib.NamedNode(`${planTableBaseUri}hasInvalidPlanLivingTogetherTableEntry`);
+const validPlanTable = new rdflib.NamedNode(`${lblodSubsidieBaseUri}validPlanLivingTogetherTable`);
+
+
+export default class CustomSubsidyFormFieldsPlanLivingTogetherTableEditComponent extends InputFieldComponent {
+  @tracked planTableSubject = null;
+  @tracked errors = [];
+  @tracked entries = [];
+
+  get hasPlanTable() {
+    if (!this.planTableSubject)
+      return false;
+    else
+      return this.storeOptions.store.match(this.sourceNode,
+        planTablePredicate,
+        this.planTableSubject,
+        this.storeOptions.sourceGraph).length > 0;
+  }
+
+  constructor() {
+    super(...arguments);
+    scheduleOnce("actions", this, this.initializeTable);
+  }
+
+  loadProvidedValue() {
+    const matches = triplesForPath(this.storeOptions);
+    const triples = matches.triples;
+    if (triples.length) {
+      this.planTableSubject = triples[0].object; // assuming only one per form
+    }
+  }
+
+  initializeTable() {
+    this.loadProvidedValue();
+
+    if(!this.hasPlanTable) {
+      this.createPlanTable();
+    }
+
+    this.validate();
+  }
+
+  createPlanTable() {
+    const uuid = uuidv4();
+    this.planTableSubject = new rdflib.NamedNode(`${planTableBaseUri}/${uuid}`);
+
+    const triples = [{
+      subject: this.planTableSubject,
+      predicate: RDF('type'),
+      object: planTableType,
+      graph: this.storeOptions.sourceGraph
+    },
+    {
+      subject: this.planTableSubject,
+      predicate: MU('uuid'),
+      object: uuid,
+      graph: this.storeOptions.sourceGraph
+    },
+    {
+      subject: this.storeOptions.sourceNode,
+      predicate: planTablePredicate,
+      object: this.planTableSubject,
+      graph: this.storeOptions.sourceGraph
+    }
+    ];
+    this.storeOptions.store.addAll(triples);
+  }
+
+  //TODO: move to some common code
+  updateTripleObject(subject, predicate, newObject = null) {
+    const triples = this.storeOptions.store.match(
+      subject,
+      predicate,
+      undefined,
+      this.storeOptions.sourceGraph
+    );
+
+    this.storeOptions.store.removeStatements([...triples]);
+
+    if (newObject) {
+      this.storeOptions.store.addAll([
+        {
+          subject: subject,
+          predicate: predicate,
+          object: newObject,
+          graph: this.storeOptions.sourceGraph
+        }
+      ]);
+    }
+  }
+
+  @action
+  validate(){
+    this.errors = [];
+
+    const entries = this.storeOptions.store.match(
+      undefined,
+      plannedRangePredicate,
+      undefined,
+      this.storeOptions.sourceGraph
+    );
+
+    const invalidRow = this.storeOptions.store.any(this.planTableSubject, hasInvalidRowPredicate, null, this.storeOptions.sourceGraph);
+    const hasPlannedRange = entries.filter(entry => parseInt(entry.object.value) > 0 );
+
+    if(!hasPlannedRange.length){
+      this.errors.pushObject({
+        message:
+          'Minstens één gepland bereik veld moet een waarde groter dan 0 bevatten.',
+      });
+      this.updateTripleObject(this.estimatedCostTableSubject, validPlanTable, null);
+    }
+    if(invalidRow){
+      this.errors.pushObject({
+        message: 'Een van de rijen is niet correct ingevuld'
+      });
+      this.updateTripleObject(this.planTableSubject, validPlanTable, null);
+    }
+    else {
+      this.updateTripleObject(this.planTableSubject, validPlanTable, true);
+    }
+  }
+
+}
---
+++ app/components/custom-subsidy-form-fields/plan-living-together-table/edit.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-submission-form-fields/components/custom-subsidy-form-fields/plan-living-together-table/edit';
