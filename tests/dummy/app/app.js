4e25757a8f2e8a2b19e4e2a02cc29313b903d397
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-04-27 18:43:49 +0200

    Use MSW to mock the case-number request
    MSW is framework agnostic and better documented than Ember's built-in mocking functionality.

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
+import { mockRequestsWorker } from 'dummy/mocks/worker';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
@@ -9,4 +10,7 @@ export default class App extends Application {
   Resolver = Resolver;
 }
 
+mockRequestsWorker.start({
+  onUnhandledRequest: 'bypass',
+});
 loadInitializers(App, config.modulePrefix);
