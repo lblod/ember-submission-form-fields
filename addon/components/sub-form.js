7e17ec0b4c52dccb0855aec1759b5e4c05af02fb
felix <ruiz.felixruiz@gmail.com>
2022-05-11 13:38:51 +0200

    Added subform-component

---
+++ addon/components/sub-form.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+import {
+  getTopLevelPropertyGroups,
+  getTopLevelListings,
+} from '../utils/model-factory';
+
+
+export default class SubFormComponent extends Component {
+  propertyGroups = []; // NOTE don't think this needs to be an ember array as it will never change
+
+  constructor() {
+    super(...arguments);
+    this.propertyGroups = getTopLevelPropertyGroups({
+      store: this.args.formStore,
+      graphs: this.args.graphs,
+      form: this.args.subForm.uri
+    });
+  }
+}
