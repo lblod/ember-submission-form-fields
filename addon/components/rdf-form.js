4ca8635c033c6468c1fe47d4cd86224772b7780c
felix <ruiz.felixruiz@gmail.com>
2022-05-11 13:32:07 +0200

    Update the useage of the API for getTopLevelPropertyGroups

--- addon/components/rdf-form.js
+++ addon/components/rdf-form.js
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
-import { getTopLevelPropertyGroups } from '../utils/model-factory';
+import {
+  getTopLevelPropertyGroups,
+  getTopLevelListings,
+} from '../utils/model-factory';
 
 export default class RdfForm extends Component {
   propertyGroups = []; // NOTE don't think this needs to be an ember array as it will never change
@@ -9,6 +12,7 @@ export default class RdfForm extends Component {
     this.propertyGroups = getTopLevelPropertyGroups({
       store: this.args.formStore,
       graphs: this.args.graphs,
+      form: this.args.form
     });
   }
 }
