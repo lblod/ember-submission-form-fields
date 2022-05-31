29fd7f0be239edeb7ebc6ae850d1ac2dbc63ac64
Oscar <lagartoverde97@gmail.com>
2022-05-31 16:51:21 +0200

    Simplify rich text component

--- addon/components/rdf-input-fields/rich-text.js
+++ addon/components/rdf-input-fields/rich-text.js
@@ -1,10 +1,34 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
+import { tracked } from '@glimmer/tracking';
 
 export default class RdfInputFieldsRichTextComponent extends SimpleInputFieldComponent {
+  @tracked editor;
   inputId = 'richtext-' + guidFor(this);
-
+  plugins = [];
+  get editorOptions() {
+    return (
+      this.args.editorOptions ?? {
+        showToggleRdfaAnnotations: false,
+        showInsertButton: false,
+        showRdfa: false,
+        showRdfaHighlight: false,
+        showRdfaHover: false,
+        showPaper: false,
+        showSidebar: false,
+      }
+    );
+  }
+  get toolbarOptions() {
+    return (
+      this.args.toolbarOptions ?? {
+        showTextStyleButtons: true,
+        showListButtons: true,
+        showIndentButtons: true,
+      }
+    );
+  }
   @action
   handleRdfaEditorInit(editor) {
     this.editor = editor;
