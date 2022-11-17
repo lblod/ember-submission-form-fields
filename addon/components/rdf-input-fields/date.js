0caab6e49f6d38683f6123b3963c19fb7ebf3af4
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-11-17 17:11:01 +0100

    Remove the custom date picker localization (#105)
    ember-appuniversum v1.10.0 added an option to localize the date picker in Dutch. This allows us to drop our own custom localization code.

--- addon/components/rdf-input-fields/date.js
+++ addon/components/rdf-input-fields/date.js
@@ -2,16 +2,10 @@ import { guidFor } from '@ember/object/internals';
 import { action } from '@ember/object';
 import { XSD } from '@lblod/submission-form-helpers';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
-import {
-  DUTCH_LOCALIZATION,
-  BELGIAN_FORMAT_ADAPTER,
-} from '@lblod/ember-submission-form-fields/config/date-picker';
 import { literal } from 'rdflib';
 
 export default class RdfInputFieldsDateComponent extends SimpleInputFieldComponent {
   inputId = 'date-' + guidFor(this);
-  localization = DUTCH_LOCALIZATION;
-  adapter = BELGIAN_FORMAT_ADAPTER;
 
   @action
   updateValue(isoDate) {
