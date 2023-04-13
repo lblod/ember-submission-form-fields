d0316cfee76c7c6423bd5125e7e67fff32331dbc
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-04-13 12:02:40 +0200

    Update Ember Data to 4.12.0 (#127)
    It seems we were using a wide version range for Ember Data and the last release made the Embroider builds fail if the built-in packageRules and adapters weren't disabled.
    
    This change updates Ember Data to 4.12 and disables the compat adapters and packageRules. Once a new version of `@embroider/compat` we can remove the config again.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,16 @@ module.exports = function (defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
 
+  // The built-in compat adapters for Ember Data shouldn't be needed anymore.
+  // This code can be removed once https://github.com/embroider-build/embroider/pull/1369 is released.
+  const compatAdapters = new Map();
+  compatAdapters.set('ember-data', null);
+  compatAdapters.set('@ember-data/adapter', null);
+  compatAdapters.set('@ember-data/model', null);
+  compatAdapters.set('@ember-data/record-data', null);
+  compatAdapters.set('@ember-data/serializer', null);
+  compatAdapters.set('@ember-data/store', null);
+
   const { maybeEmbroider } = require('@embroider/test-setup');
   return maybeEmbroider(app, {
     skipBabel: [
@@ -27,5 +37,22 @@ module.exports = function (defaults) {
         package: 'qunit',
       },
     ],
+    packageRules: [
+      {
+        package: '@ember-data/store',
+        addonModules: {
+          '-private.js': {
+            dependsOnModules: [],
+          },
+          '-private/system/core-store.js': {
+            dependsOnModules: [],
+          },
+          '-private/system/model/internal-model.js': {
+            dependsOnModules: [],
+          },
+        },
+      },
+    ],
+    compatAdapters,
   });
 };
