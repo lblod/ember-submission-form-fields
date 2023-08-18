43c2319e34d5381c29cbf976657408870f8e4c2a
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-08-18 17:03:47 +0200

    Resolve stylelint errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,4 +2,7 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'selector-class-pattern': null, // This rule enforces kebab-case by default, but Appuniversum use BEM, which isn't compatible.
+  },
 };
