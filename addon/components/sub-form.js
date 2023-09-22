f45711e85dab8de7e62a4f47bc025726813a1469
Arnie <arne@inuits.eu>
2023-09-22 13:25:30 +0200

    feat: create base of collapsable fields
    Refs: SFB-17

--- addon/components/sub-form.js
+++ addon/components/sub-form.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { getTopLevelPropertyGroups } from '../utils/model-factory';
 import isLast from '@lblod/ember-submission-form-fields/-private/helpers/is-last';
 import OrderButtonGroup from '@lblod/ember-submission-form-fields/components/listing/order-button-group';
+import { FORM, SHACL } from '@lblod/submission-form-helpers';
 
 export default class SubFormComponent extends Component {
   propertyGroups = []; // NOTE don't think this needs to be an ember array as it will never change
@@ -14,7 +15,43 @@ export default class SubFormComponent extends Component {
       store: this.args.formStore,
       graphs: this.args.graphs,
       form: this.args.subForm.uri,
+      node: this.args.sourceNode,
     });
+  }
+
+  get name() {
+    let name = this.args.formStore
+    .any(this.args.sourceNode, 
+        SHACL('name'), 
+        undefined, 
+        this.args.graphs.sourceGraph);
+
+    return name;
+  }
+
+  get collapsible() {
+    let propertyGroup = this.args.formStore
+    .any(undefined, 
+        FORM('each'), 
+        this.args.subForm.uri, 
+        this.args.graphs.formGraph);
+
+    let canCollapse = this.args.formStore
+    .any(propertyGroup,
+        FORM('canCollapse'),
+        undefined,
+        this.args.graphs.formGraph);
+
+    if(!canCollapse){
+      return false;
+    }
+
+    if(canCollapse.value === '1'){
+      return true;
+      
+    }else{
+      return false;
+    }
   }
 
   get level() {
