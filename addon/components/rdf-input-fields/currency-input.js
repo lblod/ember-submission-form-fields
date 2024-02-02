d8ed54eac1ae792e8954ca6363793d10588e145e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-02 17:21:15 +0100

    wip

--- addon/components/rdf-input-fields/currency-input.js
+++ addon/components/rdf-input-fields/currency-input.js
@@ -15,25 +15,59 @@ export default class RdfInputFieldsCurrencyInputComponent extends SimpleInputFie
     if (e && typeof e.preventDefault === 'function') e.preventDefault();
     const inputAmount = e.target.value.trim();
     this.value = this.formatAmount(inputAmount);
+
     super.updateValue(this.value);
   }
 
   formatAmount(amount) {
-    const beforeDecimalSeparator = this.getValueBeforeDecimalSeperator(amount);
-    const afterDecimalSeparator = this.getValueAfterDecimalSeperator(amount);
+    const workAmount = amount;
+    const beforeDecimalSeparator =
+      this.getValueBeforeDecimalSeperator(workAmount);
+    console.log({ beforeDecimalSeparator });
+    const afterDecimalSeparator =
+      this.getValueAfterDecimalSeperator(workAmount);
+    console.log({ afterDecimalSeparator });
 
     return (
-      beforeDecimalSeparator + this.decimalSeparator + afterDecimalSeparator
+      this.formatValueBeforeDecimalSeparator(beforeDecimalSeparator) +
+      this.decimalSeparator +
+      afterDecimalSeparator
     );
   }
 
   getValueBeforeDecimalSeperator(amount) {
-    return amount.slice(0, amount.lastIndexOf(this.decimalSeparator));
+    const lastIndex = amount.lastIndexOf(this.decimalSeparator);
+
+    return lastIndex == -1 ? amount : amount.slice(0, lastIndex);
   }
   getValueAfterDecimalSeperator(amount) {
-    return amount.slice(
-      amount.lastIndexOf(this.decimalSeparator) + 1,
-      amount.length
-    );
+    const lastIndex = amount.lastIndexOf(this.decimalSeparator);
+
+    return lastIndex == -1 ? '00' : amount.slice(lastIndex + 1, amount.length);
+  }
+
+  formatValueBeforeDecimalSeparator(amount) {
+    amount.replace(this.thousandSeparator, '');
+
+    if (amount.length > 3) {
+      amount = this.replaceEveryXCharacterWith(amount);
+    }
+
+    return amount;
+  }
+
+  replaceEveryXCharacterWith(amount) {
+    const everyXChar = 3;
+    const resultArray = [];
+
+    for (
+      let i = 0, stringLength = amount.length;
+      i < stringLength;
+      i -= everyXChar
+    ) {
+      resultArray.push(amount.substr(i, everyXChar));
+    }
+
+    return resultArray.join(this.thousandSeparator);
   }
 }
