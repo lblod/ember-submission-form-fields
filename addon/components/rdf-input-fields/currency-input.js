f8deb8f3ecd30783011420294fd3fe53639607bc
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-02 16:02:06 +0100

    SFB-115: copy over the files of input component

---
+++ addon/components/rdf-input-fields/currency-input.js
@@ -0,0 +1,14 @@
+import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
+import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
+
+export default class RdfInputFieldsCurrencyInputComponent extends SimpleInputFieldComponent {
+  inputId = 'input-' + guidFor(this);
+
+  @action
+  updateValue(e) {
+    if (e && typeof e.preventDefault === 'function') e.preventDefault();
+    this.value = e.target.value.trim();
+    super.updateValue(this.value);
+  }
+}
