6d7a7719ec030b6e2f3d9ae4a114b119fcdf3a60
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-06 11:58:36 +0100

    SFB-115: update the database value to the number value instead of the string value

--- addon/components/rdf-input-fields/currency-input.js
+++ addon/components/rdf-input-fields/currency-input.js
@@ -1,16 +1,25 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
+import { stringAmountToNumber } from '../../utils/string-amount-to-number';
 
 export default class RdfInputFieldsCurrencyInputComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
 
   currencyIsocode = 'EUR';
+  decimalSeparator = '.';
+  thousandSeparator = ' ';
 
   @action
   updateValue(e) {
     if (e && typeof e.preventDefault === 'function') e.preventDefault();
-    this.value = e.target.value.trim();
+
+    if (e.target && e.target.value) {
+      this.value = stringAmountToNumber(e.target.value, {
+        decimalSeparator: this.decimalSeparator,
+        thousandSeparator: this.thousandSeparator,
+      });
+    }
 
     super.updateValue(this.value);
   }
