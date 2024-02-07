c9523bf7a461f3c908d56902ae3cb24d27a02311
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-07 16:26:29 +0100

    SFB-115: stable editing of the number

--- addon/components/rdf-input-fields/currency-input.js
+++ addon/components/rdf-input-fields/currency-input.js
@@ -1,40 +1,20 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
-import { stringAmountToNumber } from '../../utils/string-amount-to-number';
 
 export default class RdfInputFieldsCurrencyInputComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
 
   currencyIsocode = 'EUR';
-  decimalSeparator = ',';
-  thousandSeparator = '.';
 
   @action
   updateValue(e) {
     if (e && typeof e.preventDefault === 'function') e.preventDefault();
 
     if (e.target && e.target.value) {
-      const amount = stringAmountToNumber(e.target.value, {
-        decimalSeparator: this.decimalSeparator,
-        thousandSeparator: this.thousandSeparator,
-      });
-      this.value = this.numberStringToFormattedString(amount);
+      this.value = e.target.inputmask.unmaskedvalue();
 
-      super.updateValue(amount);
+      super.updateValue(this.value);
     }
-  }
-
-  numberStringToFormattedString(numberString) {
-    const fromDecimalSeparator = '.';
-
-    return `${numberString}`.replace(
-      fromDecimalSeparator,
-      this.decimalSeparator
-    );
-  }
-
-  get formattedValue() {
-    return this.numberStringToFormattedString(this.value);
   }
 }
