f021ab16f43233a00ae280710701b7afb23dea67
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-02-16 14:35:38 +0100

    Update to MSW v2

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,7 +2,6 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
-import { mockRequestsWorker } from 'dummy/mocks/worker';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
@@ -10,7 +9,4 @@ export default class App extends Application {
   Resolver = Resolver;
 }
 
-mockRequestsWorker.start({
-  onUnhandledRequest: 'bypass',
-});
 loadInitializers(App, config.modulePrefix);
