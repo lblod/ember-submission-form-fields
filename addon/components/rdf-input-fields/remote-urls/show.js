81c113c6ecc7bb0f59917fb75dcc3a3285a6b6c1
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-02-28 11:10:01 +0100

    Remove the `InputField` base class from the `RemoteUrls::show` component
    We don't use any of the base functionality, other than the storeOptions getter. Removing the base class makes things easier to reason about.

--- addon/components/rdf-input-fields/remote-urls/show.js
+++ addon/components/rdf-input-fields/remote-urls/show.js
@@ -1,39 +1,54 @@
-import InputFieldComponent from '../input-field';
+import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { triplesForPath } from '@lblod/submission-form-helpers';
 import { guidFor } from '@ember/object/internals';
-import { A } from '@ember/array';
 
 import { RDF } from '@lblod/submission-form-helpers';
 import { NamedNode } from 'rdflib';
 
-export default class FormInputFieldsRemoteUrlsShowComponent extends InputFieldComponent {
+export default class FormInputFieldsRemoteUrlsShowComponent extends Component {
   inputId = 'remote-urls-' + guidFor(this);
 
   @service store;
 
-  @tracked remoteUrls = A();
+  @tracked remoteUrls;
   @tracked hasRemoteUrlErrors = false;
 
   constructor() {
     super(...arguments);
-    this.loadProvidedValue();
+    this.loadRemoteUrls();
   }
 
-  async loadProvidedValue() {
+  get storeOptions() {
+    return {
+      formGraph: this.args.graphs.formGraph,
+      sourceGraph: this.args.graphs.sourceGraph,
+      metaGraph: this.args.graphs.metaGraph,
+      sourceNode: this.args.sourceNode,
+      store: this.args.formStore,
+      path: this.args.field.rdflibPath,
+      scope: this.args.field.rdflibScope,
+    };
+  }
+
+  async loadRemoteUrls() {
     const matches = triplesForPath(this.storeOptions);
+    const remoteUrls = [];
 
     for (let uri of matches.values) {
       try {
         if (this.isRemoteDataObject(uri)) {
           const record = await this.loadRemoteDataObjectRecord(uri);
-          this.remoteUrls.pushObject(record);
+          remoteUrls.push(record);
         }
       } catch (error) {
+        console.error(error);
         this.hasRemoteUrlErrors = true;
       }
     }
+
+    this.remoteUrls = remoteUrls;
   }
 
   isRemoteDataObject(subject) {
@@ -41,11 +56,11 @@ export default class FormInputFieldsRemoteUrlsShowComponent extends InputFieldCo
       'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject'
     );
     return (
-      this.storeOptions.store.match(
+      this.args.formStore.match(
         subject,
         RDF('type'),
         remoteDataObjectType,
-        this.storeOptions.sourceGraph
+        this.args.sourceGraph
       ).length > 0
     );
   }
