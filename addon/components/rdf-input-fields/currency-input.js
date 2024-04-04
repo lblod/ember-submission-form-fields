f8fc91aaa38a1c4a0af492f8448a1573d68bed8c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-04 13:31:45 +0200

    SFB-224: using get datatype getter from numerical input
    using this as when a user fills in the number zero an error was shown as the value was not written to the path as value number zero

--- addon/components/rdf-input-fields/currency-input.js
+++ addon/components/rdf-input-fields/currency-input.js
@@ -1,6 +1,8 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
+import { XSD } from '@lblod/submission-form-helpers';
+import { literal } from 'rdflib';
 
 export default class RdfInputFieldsCurrencyInputComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
@@ -9,13 +11,30 @@ export default class RdfInputFieldsCurrencyInputComponent extends SimpleInputFie
 
   @action
   updateValue(e) {
+    let updatedValue = null;
     if (e && typeof e.preventDefault === 'function') e.preventDefault();
 
     if (e.target && e.target.value) {
       this.value = e.target.inputmask.unmaskedvalue();
-    } else {
-      this.value = null;
+      updatedValue = literal(Number(this.value), this.datatype);
     }
-    super.updateValue(this.value);
+
+    super.updateValue(updatedValue);
+  }
+
+  // Took this over from numerical-input.js
+  // When value 0 is filled in this added as that value
+  // When just updating the value with Number(0) no value is written to the path
+  get datatype() {
+    const number = Number(this.value);
+    if (!Number.isNaN(number) && Number.isFinite(number)) {
+      let datatype = XSD('decimal');
+      if (Number.isInteger(number) && Number.isSafeInteger(number)) {
+        datatype = XSD('integer');
+      }
+      return datatype;
+    }
+    // NOTE: everything that is not a number is a string.
+    return XSD('string');
   }
 }
