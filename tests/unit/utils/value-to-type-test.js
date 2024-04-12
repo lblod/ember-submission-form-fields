7bbf15379baff93607fdfea00a3a86db746aa24a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-12 14:41:37 +0200

    LMB-326: cleanup test file

--- tests/unit/utils/value-to-type-test.js
+++ tests/unit/utils/value-to-type-test.js
@@ -4,7 +4,7 @@ import { valueToType } from '@lblod/ember-submission-form-fields/utils/value-to-
 import { isNamedNode } from 'rdflib';
 
 module('Unit | Utility | value to type', function () {
-  test('Can proccess a value of type function to the possible returntypes', function (assert) {
+  test('Can proccess a value of type `function to the possible returntypes', function (assert) {
     const countOneAndOne = () => {
       return 1 + 1;
     };
@@ -13,11 +13,14 @@ module('Unit | Utility | value to type', function () {
     const valueAsBoolean = valueToType(countOneAndOne, 'boolean');
 
     assert.deepEqual(typeof countOneAndOne, 'function');
+
     assert.deepEqual(typeof valueAsString, 'string');
+
     assert.deepEqual(typeof valueAsBoolean, 'boolean');
     assert.false(valueAsBoolean);
   });
-  test('Can proccess a value of type object to the possible returntypes', function (assert) {
+
+  test('Can proccess a value of type `object` to the possible returntypes', function (assert) {
     const myObject = {
       property: 'value',
     };
@@ -26,36 +29,45 @@ module('Unit | Utility | value to type', function () {
     const valueAsBoolean = valueToType(myObject, 'boolean');
 
     assert.deepEqual(typeof myObject, 'object');
+
     assert.deepEqual(typeof valueAsString, 'string');
+
     assert.deepEqual(typeof valueAsBoolean, 'boolean');
     assert.false(valueAsBoolean);
   });
-  test('Can proccess a value of type string to the possible returntypes', function (assert) {
+
+  test('Can proccess a value of type `string` to the possible returntypes', function (assert) {
     const myString = 'qunit';
 
     const valueAsString = valueToType(myString, 'string');
     const valueAsBoolean = valueToType(myString, 'boolean');
 
     assert.deepEqual(typeof myString, 'string');
+
     assert.deepEqual(typeof valueAsString, 'string');
     assert.deepEqual(valueAsString, myString);
+
     assert.deepEqual(typeof valueAsBoolean, 'boolean');
     assert.false(valueAsBoolean);
   });
-  test('Can proccess a value of type boolean to the possible returntypes', function (assert) {
+
+  test('Can proccess a value of type `boolean` to the possible returntypes', function (assert) {
     const myBooleanTrue = true;
 
     const valueAsString = valueToType(myBooleanTrue, 'string');
     const valueAsBoolean = valueToType(myBooleanTrue, 'boolean');
 
     assert.deepEqual(typeof myBooleanTrue, 'boolean');
+
     assert.deepEqual(typeof valueAsString, 'string');
     assert.deepEqual(valueAsString, `${myBooleanTrue}`);
+
     assert.deepEqual(typeof valueAsBoolean, 'boolean');
     assert.deepEqual(valueAsBoolean, myBooleanTrue);
     assert.true(valueAsBoolean);
   });
-  test('When string value ois zero or one it can be converte to a boolean true/false', function (assert) {
+
+  test('When string value is zero or one it can be converte to a boolean true/false', function (assert) {
     const stringZero = '0';
     const stringOne = '1';
 
@@ -70,12 +82,14 @@ module('Unit | Utility | value to type', function () {
     assert.false(zeroToFalse);
     assert.true(oneToTrue);
   });
+
   test('When  passing a uri it can convert it to a node', function (assert) {
     const uri = `https://semantic.works/`;
 
     const uriNode = valueToType(uri, 'node');
 
     assert.deepEqual(typeof uri, 'string');
+
     assert.true(isNamedNode(uriNode));
     assert.deepEqual(uriNode.value, uri);
   });
