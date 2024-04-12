ee8492d0db4db940cc3d835d3bd5bc0fdaa277d8
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-12 15:52:39 +0200

    LMB-326: add comment for the alert component that im not changing it here

--- addon/components/rdf-input-fields/alert.js
+++ addon/components/rdf-input-fields/alert.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 
 export default class AlertComponent extends Component {
   get options() {
+    // Not changin this to findFieldOption() See input-field.js
     return this.args.field.options;
   }
 }
