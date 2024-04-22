4d6fd94726962ed6e2d5416958b1eb86f3938949
Ben Fidlers <ben.fidlers@qmino.com>
2024-04-22 15:06:36 +0200

    always allow clearing a numerical-input

--- addon/components/rdf-input-fields/numerical-input.js
+++ addon/components/rdf-input-fields/numerical-input.js
@@ -1,45 +1,17 @@
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
 import { XSD } from '@lblod/submission-form-helpers';
-import { Literal, literal } from 'rdflib';
-import { hasValidFieldOptions } from '@lblod/ember-submission-form-fields/utils/has-valid-field-options';
-import { FORM_OPTION } from '@lblod/ember-submission-form-fields/utils/namespaces';
+import { literal } from 'rdflib';
 
 export default class RdfInputFieldsNumericalInputComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
-
-  @tracked allowClear;
-
-  constructor() {
-    super(...arguments);
-
-    this.loadOptions();
-  }
-
-  loadOptions() {
-    let allowClear = this.args.formStore.any(
-      this.args.field.uri,
-      FORM_OPTION('allowClear'),
-      undefined,
-      this.args.graphs.formGraph
-    );
-    if (!allowClear) {
-      if (!hasValidFieldOptions(this.args.field, ['allowClear'])) {
-        this.allowClear = false;
-      }
-      this.allowClear = this.args.field.options.allowClear;
-    } else {
-      this.allowClear = Literal.toJS(allowClear);
-    }
-  }
 
   @action
   updateValue(e) {
     if (e && typeof e.preventDefault === 'function') e.preventDefault();
     this.value = e.target.value;
-    if (!this.value && this.allowClear) {
+    if (!this.value) {
       this.value = null;
       super.updateValue(null);
     } else {
