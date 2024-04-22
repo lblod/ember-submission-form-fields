f1336c98e19453b0347829033a1004c8b3e2d666
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-22 12:05:21 +0200

    fix: uncomment the beforeModel in application route

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,9 +2,9 @@ import Route from '@ember/routing/route';
 import { mockRequestsWorker } from 'dummy/mocks/worker';
 
 export default class ApplicationRoute extends Route {
-  // beforeModel() {
-  //   return mockRequestsWorker.start({
-  //     onUnhandledRequest: 'bypass',
-  //   });
-  // }
+  beforeModel() {
+    return mockRequestsWorker.start({
+      onUnhandledRequest: 'bypass',
+    });
+  }
 }
