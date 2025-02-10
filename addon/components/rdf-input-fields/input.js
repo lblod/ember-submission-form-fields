e0dd88d085404bfb7b2dbb0c04a0ae323ffca6c3
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-02-10 17:57:49 +0100

    Support the `helpTextBelowLabel` option in some fields

--- addon/components/rdf-input-fields/input.js
+++ addon/components/rdf-input-fields/input.js
@@ -1,9 +1,11 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
+import HelpText from '@lblod/ember-submission-form-fields/components/private/help-text';
 
 export default class RdfInputFieldsInputComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
+  HelpText = HelpText;
 
   @action
   updateValue(e) {
