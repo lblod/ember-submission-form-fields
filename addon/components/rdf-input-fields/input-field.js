fd80de3b18b83d7a3c39f5e8dfd044ad4b612646
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-06-11 17:14:19 +0200

    lmb-1513: update validations handles the validations + adds a loading state so the errors do not show while running

--- addon/components/rdf-input-fields/input-field.js
+++ addon/components/rdf-input-fields/input-field.js
@@ -20,6 +20,7 @@ export default class InputFieldComponent extends Component {
   @tracked warningValidations = [];
 
   @tracked hasBeenFocused = false;
+  @tracked isLoadingValidations = false;
 
   constructor() {
     super(...arguments);
@@ -27,6 +28,10 @@ export default class InputFieldComponent extends Component {
   }
 
   get canShowValidationMessages() {
+    if (this.isLoadingValidations) {
+      return false;
+    }
+
     return this.hasBeenFocused || this.args.forceShowErrors;
   }
 
@@ -115,16 +120,20 @@ export default class InputFieldComponent extends Component {
   }
 
   updateValidations() {
-    this.errorValidations = invalidResults(
+    this.isLoadingValidations = true;
+    Promise.all([
       validationResultsForField(this.args.field.uri, this.storeOptions),
-    );
-
-    this.warningValidations = invalidResults(
       validationResultsForField(this.args.field.uri, {
         ...this.storeOptions,
         severity: SH_WARNING,
       }),
-    );
+    ])
+      .then(([errors, warnings]) => {
+        this.errorValidations = invalidResults(errors);
+        this.warningValidations = invalidResults(warnings);
+        this.isLoadingValidations = false;
+      })
+      .catch(() => (this.isLoadingValidations = false));
   }
 }
 
