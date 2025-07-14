19c3aec687b073c2fc4c181646b8b5e0cc3b4085
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-07-14 13:47:23 +0200

    Drop support for ember-concurrency < v4

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ module.exports = {
   name: require('./package').name,
   options: {
     babel: {
-      plugins: [],
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
     },
     '@embroider/macros': {
       setOwnConfig: {
@@ -13,30 +15,12 @@ module.exports = {
     },
   },
 
-  init() {
-    this._super.init.apply(this, arguments);
-    this.maybeAddConcurrencyPlugin();
-  },
-
   included: function (app) {
     this._super.included.apply(this, arguments);
     let addonOptions = app.options[this.name];
     if (addonOptions) {
       let ownConfig = this.options['@embroider/macros'].setOwnConfig;
       Object.assign(ownConfig, addonOptions);
-    }
-  },
-
-  maybeAddConcurrencyPlugin() {
-    const VersionChecker = require('ember-cli-version-checker');
-    const checker = new VersionChecker(this.project);
-    const dep = checker.for('ember-concurrency');
-
-    if (dep.gte('4.0.0')) {
-      // ember-concurrency v4+ requires a custom babel transform. Once we drop ember-concurrency v3 support we can remove this conditional registration.
-      this.options.babel.plugins.push(
-        require.resolve('ember-concurrency/async-arrow-task-transform'),
-      );
     }
   },
 };
