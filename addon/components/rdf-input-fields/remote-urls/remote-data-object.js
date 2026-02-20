a2b4316495696150215c7689f2b704faeb477538
benjay10 <ben.behaeghe@redpencil.io>
2026-02-20 05:14:05 +0100

    Use local helpers instead of model getters

--- addon/components/rdf-input-fields/remote-urls/remote-data-object.js
+++ addon/components/rdf-input-fields/remote-urls/remote-data-object.js
@@ -1,5 +1,12 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import * as filehelpers from '@lblod/ember-submission-form-fields/-private/helpers/file';
+import * as remoteDataObjectHelpers from '@lblod/ember-submission-form-fields/-private/helpers/remoteDataObject';
 
 export default class FormInputFieldsRemoteUrlsRemoteDataObjectComponent extends Component {
+  humanReadableSize = filehelpers.humanReadableSize;
+  extensionFormatted = filehelpers.extensionFormatted;
+  filenameWithoutExtension = filehelpers.filenameWithoutExtension;
+  downloadLink = remoteDataObjectHelpers.downloadLink;
+  downloadSuccess = remoteDataObjectHelpers.downloadSuccess;
 }
